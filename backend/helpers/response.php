2bd0a29b8ec45ad32226a23ded802378f0a16aed
kyen-p <pkyen1209@gmail.com>
2026-08-28 15:48:46 +0700

    xóa

--- backend/helpers/response.php
+++
@@ -1,6 +0,0 @@
-<?php
-function jsonResponse($success, $data = null, $message = "") {
-    header('Content-Type: application/json');
-    echo json_encode(["success" => $success, "message" => $message, "data" => $data]);
-    exit;
-}
