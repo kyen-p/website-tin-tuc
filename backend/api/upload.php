6217d9a44ff32fb280ae550d675f8115a5dd8a40
TrinhPhuong <trinhphuong26052006@gmail.com>
2026-09-01 13:16:55 +0700

    tạo

--- backend/api/upload.php
+++ backend/api/upload.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once '../config/database.php';
-require_once '../helpers/response.php';
-require_once '../helpers/auth.php';
+require_once __DIR__ . '/../../config/database.php';
+require_once __DIR__ . '/../../helpers/response.php';
+require_once __DIR__ . '/../../helpers/auth.php';
 
 requireLogin();
 
